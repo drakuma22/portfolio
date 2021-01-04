2cc930c50a64c7209505078b38278f4500b148cf
drakuma22 <63706319+drakuma22@users.noreply.github.com>
2021-01-04 17:41:47 +0100

    Button scrolling, title script, smooth effect and github project link

--- portfolio/js/button.js
+++ portfolio/js/button.js
@@ -1,6 +1,8 @@
 mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function(){
+  scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -8,9 +10,9 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
-}
+};
 
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
---
+++ portfolio/js/title.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.animate', {
+strings:[
+ "Sviluppatore software",
+ "Web Designer",
+ "Appassionato di tecnologia"
+],
+typedSpeed: 50,
+backSpeed: 50,
+loop: true
+});
